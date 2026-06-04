86deb42dcdede7a16f59166bb51518f59f32e365
gissellgelvis-maker <gissellgelvis@gmail.com>
2026-06-04 10:04:58 -0500

    Migraciones_de_cargos, funciones_cargo y empleados

---
+++ database/migrations/2026_06_03_202331_create__funciones_cargo_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('funciones_cargo', function (Blueprint $table) {
+            $table->id('id_funcion');
+            $table->string('descripcion_funcion');
+            $table->string('estado');
+            $table->unsignedBigInteger('id_cargo');
+            $table->foreign('id_cargo')->references('id_cargo')->on('cargos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funciones_cargo');
+    }
+};
